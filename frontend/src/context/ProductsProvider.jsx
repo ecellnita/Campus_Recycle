e8913885c7ff08db4b450d4e8819481e20fd9856
swaparup36 <swaparup36@gmail.com>
2025-11-07 00:17:19 +0530

    be server change

--- frontend/src/context/ProductsProvider.jsx
+++ frontend/src/context/ProductsProvider.jsx
@@ -14,7 +14,7 @@ export const ProductProvider = (props) => {
 
   const getAllProducts = async() => {
     try {
-      const response = await axios.post('https://campus-recycle.onrender.com/api/v1/product/getallproduct', {
+      const response = await axios.post('https://backend.recycool.ecellnita.in/api/v1/product/getallproduct', {
         headers: { Authorization: `Bearer ${localStorage.getItem('campusrecycletoken')}` }
       });
       // const response = await axios.post('http://localhost:4000/api/v1/product/getallproduct', {
